dcf2cccf40b11028a654dc77822215ea342d764c
Bartek <badamiec@st.swps.edu.pl>
2024-12-07 23:10:06 +0100

    zadania_lab06

--- folder_projektowy/folder_projektowy/settings.py
+++ folder_projektowy/folder_projektowy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'folder_aplikacji', #folder_aplikacji.app.FolderAplikacjiConfig
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
