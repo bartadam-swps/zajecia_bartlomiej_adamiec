5f3345d5f4b7a9345d2b91182a2f7647d80c6376
Bartek <badamiec@st.swps.edu.pl>
2024-12-08 20:50:40 +0100

    zadania_cz2

--- folder_projektowy/folder_aplikacji/views.py
+++ folder_projektowy/folder_aplikacji/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Person, Team, Osoba, Stanowisko
 from .serializers import PersonSerializer, OsobaSerializer, StanowiskoSerializer
 
@@ -118,4 +119,39 @@ def stanowisko_detail(request, pk):
     
     elif request.method == 'DELETE':
         stanowisko.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    
+    
+class OsobaList(APIView):
+    def get(sellf, request):
+        osoby = Stanowisko.objects.all()
+        serializer = StanowiskoSerializer(osoby, many = True)
+        return Response(serializer.data)
+    
+    def post(sellf, request):
+        if request.method == 'POST':
+            serializer = StanowiskoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+class OsobaDetail(APIView):
+    def get(sellf, request):
+        try:
+            osoba = Osoba.objects.get(pk=pk)
+        except Osoba.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = OsobaSerializer(osoba)
+        return Response(serializer.data)
+    
+    
+    def delete(self, request, pk):
+        try:
+            osoba = Osoba.objects.get(pk=pk)
+        except Osoba.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        osoba.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
